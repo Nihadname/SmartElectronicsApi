95f32a78c7e040511b3ee83cbb1a1eaf1a9bba47
Nihadname <nihadcoding@gmail.com>
2024-09-10 02:29:32 +0400

    done

---
+++ SmartElectronicsApi.Api/Middlewares/CustomExceptionMiddleware.cs
@@ -0,0 +1,37 @@
+﻿using SmartElectronicsApi.Application.Exceptions;
+
+namespace SmartElectronicsApi.Api.Middlewares
+{
+    public class CustomExceptionMiddleware
+    {
+        private readonly RequestDelegate _Next;
+
+        public CustomExceptionMiddleware(RequestDelegate next)
+        {
+            _Next = next;
+        }
+        public async Task InvokeAsync(HttpContext httpContext)
+        {
+            try
+            {
+                await _Next.Invoke(httpContext);
+            }
+            catch (Exception ex)
+            {
+                var message = ex.Message;
+                var errors = new Dictionary<string, string>();
+                httpContext.Response.StatusCode = 500;
+                if (ex is CustomException custom)
+                {
+                    message = custom.Message;
+                    errors = custom.Errors;
+                    httpContext.Response.StatusCode = custom.Code;
+
+                }
+                await httpContext.Response.WriteAsJsonAsync(new { message, errors });
+
+
+            }
+        }
+    }
+}
---
+++ SmartElectronicsApi.Core/Repositories/IRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartElectronicsApi.Core.Repositories
+{
+    public interface IRepository<T> where T : class
+    {
+        Task<T> GetEntity(Expression<Func<T, bool>> predicate = null, params string[] includes);
+        Task<List<T>> GetAll(Expression<Func<T, bool>> predicate = null, params string[] includes);
+        Task Create(T entity);
+        Task Update(T entity);
+        Task Delete(T entity);
+        Task<bool> isExists(Expression<Func<T, bool>> predicate = null);
+        Task Commit();
+    }
+}
