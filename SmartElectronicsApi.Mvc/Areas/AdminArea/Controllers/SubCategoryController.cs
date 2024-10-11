38b2e8ddb14f0b1bd0089e709297bfcc8ec24aa1
Nihadname <nihadcoding@gmail.com>
2024-10-12 03:08:15 +0400

    done

---
+++ SmartElectronicsApi.Mvc/Areas/AdminArea/Controllers/SubCategoryController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SmartElectronicsApi.Mvc.ViewModels.Color;
+using SmartElectronicsApi.Mvc.ViewModels.pagination;
+using System.Net.Http.Headers;
+using System.Net;
+using SmartElectronicsApi.Mvc.ViewModels.SubCategory;
+
+namespace SmartElectronicsApi.Mvc.Areas.AdminArea.Controllers
+{
+    [Area("AdminArea")]
+    public class SubCategoryController : Controller
+    {
+        public async Task<IActionResult> Index(int pageNumber = 1, int pageSize = 2)
+        {
+            using var client = new HttpClient();
+            client.BaseAddress = new Uri("http://localhost:5246/api/");
+            client.DefaultRequestHeaders.Authorization =
+      new AuthenticationHeaderValue("Bearer", Request.Cookies["JwtToken"]);
+            using HttpResponseMessage httpResponseMessage = await client.GetAsync($"http://localhost:5246/api/SubCategory?pageNumber={pageNumber}&pageSize={pageSize}");
+            if (httpResponseMessage.IsSuccessStatusCode)
+            {
+                string ContentStream = await httpResponseMessage.Content.ReadAsStringAsync();
+                var data = JsonConvert.DeserializeObject<PaginatedResponseVM<SubCategoryListItemVM>>(ContentStream);
+                return View(data);
+            }
+            else if (httpResponseMessage.StatusCode == HttpStatusCode.Unauthorized)
+            {
+                return RedirectToAction("Login", "Account", new { area = "" });
+            }
+            else if (httpResponseMessage.StatusCode == HttpStatusCode.Forbidden)
+            {
+                return RedirectToAction("AccessDenied", "Account", new { area = "" });
+            }
+            else
+            {
+                return RedirectToAction("Error404", "Home", new { area = "" });
+            }
+        }
+    }
+}
